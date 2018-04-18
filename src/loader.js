1a99f684edd2145bd60b8d73041519bd193a5e76
bailnl <bailnl@qq.com>
2018-04-19 00:08:46 +0800

    add module.exports

--- src/loader.js
+++ src/loader.js
@@ -3,7 +3,10 @@ const fs = require('fs')
 module.exports = function rawContentLoader() {
   const source = fs.readFileSync(this.resourcePath, "utf-8").toString();
 
-  return JSON.stringify(source)
+
+  const json = JSON.stringify(source)
     .replace(/\u2028/g, '\\u2028')
-    .replace(/\u2029/g, '\\u2029')
+    .replace(/\u2029/g, '\\u2029');
+
+  return `module.exports = ${json}`;
 };
